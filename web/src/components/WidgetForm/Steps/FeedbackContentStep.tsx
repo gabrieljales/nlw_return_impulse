4c66c9c0a216ebf1132bf276d9a15456d62cf030
Gabriel_Jales <gabrielnascimentojaless@gmail.com>
2022-05-19 18:41:11 -0300

    Submit feedback

--- web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,5 +1,5 @@
 import { ArrowLeft, Camera } from "phosphor-react";
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 import { FeedbackType, feedbackTypes } from "..";
 import { CloseButton } from "../../CloseButton";
 import { ScreenshotButton } from "../ScreenshotButton";
@@ -14,6 +14,16 @@ export function FeedbackContentStep({
     onFeedbackRestartRequested,
 }: FeedbackContentStepProps) {
     const [screenshot, setScreenshot] = useState<string | null>(null);
+    const [comment, setComment] = useState('');
+
+    function handleSubmitFeedback(event: FormEvent) {
+        event.preventDefault(); // Prevenir o comportamento padrão dos forms de dar reload na página
+
+        console.log({
+            screenshot,
+            comment,
+        })
+    }
 
     const feedbackTypeInfo = feedbackTypes[feedbackType];
 
@@ -40,10 +50,11 @@ export function FeedbackContentStep({
                 <CloseButton />
             </header>
 
-            <form className="my-4 w-full">
+            <form onSubmit={handleSubmitFeedback} className="my-4 w-full">
                 <textarea
                     className="min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-none resize-none scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin"
                     placeholder="Conte com detalhes o que está acontecendo..."
+                    onChange={event => setComment(event.target.value)}
                 ></textarea>
 
                 <footer className="flex gap-2 mt-2">
