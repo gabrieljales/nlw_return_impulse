6dacf4e917024cc69618ae09888f4a0f91d85293
Gabriel_Jales <gabrielnascimentojaless@gmail.com>
2022-05-19 18:33:34 -0300

    screenshot button done

--- web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,59 +1,65 @@
 import { ArrowLeft, Camera } from "phosphor-react";
+import { useState } from "react";
 import { FeedbackType, feedbackTypes } from "..";
 import { CloseButton } from "../../CloseButton";
 import { ScreenshotButton } from "../ScreenshotButton";
 
 interface FeedbackContentStepProps {
-  feedbackType: FeedbackType;
-  onFeedbackRestartRequested: () => void;
+    feedbackType: FeedbackType;
+    onFeedbackRestartRequested: () => void;
 }
 
 export function FeedbackContentStep({
-  feedbackType,
-  onFeedbackRestartRequested,
+    feedbackType,
+    onFeedbackRestartRequested,
 }: FeedbackContentStepProps) {
-  const feedbackTypeInfo = feedbackTypes[feedbackType];
+    const [screenshot, setScreenshot] = useState<string | null>(null);
 
-  return (
-    <>
-      <header>
-        <button
-          type="button"
-          className="top-5 left-5 absolute text-zinc-400 hover:text-zinc-100"
-          onClick={onFeedbackRestartRequested}
-        >
-          <ArrowLeft weight="bold" className="w-4 h-4" />
-        </button>
+    const feedbackTypeInfo = feedbackTypes[feedbackType];
 
-        <span className="text-xl leading-6 flex items-center gap-2">
-          <img
-            src={feedbackTypeInfo.image.source}
-            alt={feedbackTypeInfo.image.alt}
-            className="w-6 h-6"
-          />
-          {feedbackTypeInfo.title}
-        </span>
+    return (
+        <>
+            <header>
+                <button
+                    type="button"
+                    className="top-5 left-5 absolute text-zinc-400 hover:text-zinc-100"
+                    onClick={onFeedbackRestartRequested}
+                >
+                    <ArrowLeft weight="bold" className="w-4 h-4" />
+                </button>
 
-        <CloseButton />
-      </header>
+                <span className="text-xl leading-6 flex items-center gap-2">
+                    <img
+                        src={feedbackTypeInfo.image.source}
+                        alt={feedbackTypeInfo.image.alt}
+                        className="w-6 h-6"
+                    />
+                    {feedbackTypeInfo.title}
+                </span>
 
-      <form className="my-4 w-full">
-        <textarea
-          className="min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-none resize-none scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin"
-          placeholder="Conte com detalhes o que está acontecendo..."
-        ></textarea>
+                <CloseButton />
+            </header>
 
-        <footer className="flex gap-2 mt-2">
-          <ScreenshotButton />
+            <form className="my-4 w-full">
+                <textarea
+                    className="min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-none resize-none scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin"
+                    placeholder="Conte com detalhes o que está acontecendo..."
+                ></textarea>
 
-          <button
-            type="submit"
-            className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors"
-          >
-            Enviar Feedback
-          </button>
-        </footer>
-      </form>
-    </>
-  );
+                <footer className="flex gap-2 mt-2">
+                    <ScreenshotButton
+                        screenshot={screenshot}
+                        onScreenshotTook={setScreenshot}
+                    />
+
+                    <button
+                        type="submit"
+                        className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors"
+                    >
+                        Enviar Feedback
+                    </button>
+                </footer>
+            </form>
+        </>
+    );
 }
